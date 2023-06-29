f68758864e426c340d460e4ba53797adfdc3a1f2
vmtuan12 <21020664@vnu.edu.vn>
2023-06-29 17:59:54 +0700

    create modal component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,58 +1,21 @@
 <script setup>
+import { ref } from 'vue';
+import Modal from '../components/UI/Modal.vue';
+import Button from '../components/UI/Button.vue'
+
+const condition = ref(false);
+
+const lasd = () => {
+    console.log(123);
+};
 </script>
 
 <template>
-    <div class="bg-white rounded-xl shadow flex flex-col items-center space-y-4 pb-4">
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
-        <p>123</p>
+    <div>
+        <div class="bg-white rounded-xl shadow flex space-y-4 p-4">
+            <Button @click="condition = true">123</Button>
+        </div>
+        <Modal :condition="condition" @exit-modal="condition = false">123</Modal>
     </div>
 </template>
 
